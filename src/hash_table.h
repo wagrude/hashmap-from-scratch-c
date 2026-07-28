4762b9bc68d03534788c4552d9e10de04ac90f01
Shivansh <shivansh.work.college@gmail.com>
2026-07-28 12:48:37 +0530

    added structs for hash

--- src/hash_table.h
+++ src/hash_table.h
@@ -3,11 +3,14 @@ typedef struct {
     char* key;
     char* value;
 } ht_item;
+
 // Our hash table stores an array of pointers to items, and some details about its size and how full it is:
 // hash_table.h
+
 typedef struct {
     int size;
     int count;
     ht_item** items;
 } ht_hash_table;
 
+// load_factor = count / size
