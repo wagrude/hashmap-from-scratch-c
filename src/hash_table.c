61465e3a4963153b2152d576a76c9fcd81964605
Shivansh <shivansh.work.college@gmail.com>
2026-07-29 13:30:05 +0530

    added hash table code in c

---
+++ src/hash_table.c
@@ -0,0 +1,12 @@
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "hash_table.h"
+
+static ht_item* ht_new_item(const char* k, const char* v) {
+    ht_item* i = malloc(sizeof(ht_item));
+    i->key = strdup(k);
+    i->value = strdup(v);
+    return i;
+}
