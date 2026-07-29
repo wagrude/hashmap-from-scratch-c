41f0ccedaa38d08636dc307dd5e4740fd6c3886e
Shivansh <shivansh.work.college@gmail.com>
2026-07-29 13:32:51 +0530

    added hash table code in c

--- src/hash_table.c
+++ src/hash_table.c
@@ -10,3 +10,15 @@ static ht_item* ht_new_item(const char* k, const char* v) {
     i->value = strdup(v);
     return i;
 }
+
+
+
+
+ht_hash_table* ht_new() {
+    ht_hash_table* ht = malloc(sizeof(ht_hash_table));
+
+    ht->size = 53;
+    ht->count = 0;
+    ht->items = calloc((size_t)ht->size, sizeof(ht_item*));
+    return ht;
+}
