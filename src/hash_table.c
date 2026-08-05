a9f60118c2821b0f0a5691b0e3fc7131e5e3e772
Shivansh <shivansh.work.college@gmail.com>
2026-08-05 18:45:53 +0530

    visual

--- src/hash_table.c
+++ src/hash_table.c
@@ -3,32 +3,38 @@
 
 #include "hash_table.h"
 
-static ht_item* ht_new_item(const char* k, const char* v) {
-    ht_item* i = malloc(sizeof(ht_item));
+static ht_item *ht_new_item(const char *k, const char *v)
+{
+    ht_item *i = malloc(sizeof(ht_item));
     i->key = strdup(k);
     i->value = strdup(v);
     return i;
 }
 
-ht_hash_table* ht_new() {
-    ht_hash_table* ht = malloc(sizeof(ht_hash_table));
+ht_hash_table *ht_new()
+{
+    ht_hash_table *ht = malloc(sizeof(ht_hash_table));
 
     ht->size = 53;
     ht->count = 0;
-    ht->items = calloc((size_t)ht->size, sizeof(ht_item*));
+    ht->items = calloc((size_t)ht->size, sizeof(ht_item *));
     return ht;
 }
 
-static void ht_del_item(ht_item* i) {
+static void ht_del_item(ht_item *i)
+{
     free(i->key);
     free(i->value);
     free(i);
 }
 
-void ht_del_hash_table(ht_hash_table* ht) {
-    for (int i = 0; i < ht->size; i++) {
-        ht_item* item = ht->items[i];
-        if (item != NULL) {
+void ht_del_hash_table(ht_hash_table *ht)
+{
+    for (int i = 0; i < ht->size; i++)
+    {
+        ht_item *item = ht->items[i];
+        if (item != NULL)
+        {
             ht_del_item(item);
         }
     }
@@ -36,11 +42,13 @@ void ht_del_hash_table(ht_hash_table* ht) {
     free(ht);
 }
 
-static int ht_hash(const char* s, const int a, const int m) {
+static int ht_hash(const char *s, const int a, const int m)
+{
     long hash = 0;
     const int len_s = strlen(s);
-    for (int i = 0; i < len_s; i++) {
-        hash += (long)pow(a, len_s - (i+1)) * s[i];
+    for (int i = 0; i < len_s; i++)
+    {
+        hash += (long)pow(a, len_s - (i + 1)) * s[i];
         hash = hash % m;
     }
     return (int)hash;
